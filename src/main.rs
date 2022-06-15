f90209d4893a259008e548cfea11ada78210a5f4
kentharit <kentharit123@gmail.com>
2022-06-15 17:26:11 +0700

    added sending to specific peers

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ use std::fs::File;
 use std::net::TcpListener;
 use std::io::BufReader;
 use std::io::prelude::*;
+use std::collections::HashMap;
 
 fn main() {
     let binding_addr = get_bind_addr();
@@ -20,17 +21,26 @@ fn main() {
     println!("Server is started");
     println!("You can try to connect to the server using telnet");
 
-    let connection_list = Arc::new(Mutex::new(Vec::<TcpStream>::new()));
+    let connection_map = Arc::new(Mutex::new(HashMap::new()));
+    // let mut count = 0;
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
+        println!("{:?}", stream);
         
-        let locked_mutex = (*connection_list).lock();
-        let mut the_vector = locked_mutex.unwrap();
-        the_vector.push(stream.try_clone().unwrap());
+        let locked_mutex = (*connection_map).lock();
+        let the_hashmap = locked_mutex.unwrap();
+
+        // let mut client_id = String::new();
+        // client_id.push_str("Client_");
+        // client_id.push_str(&count.to_string());
+
+        // the_hashmap.insert(client_id, stream.try_clone().unwrap());
+
+        // count += 1;
 
         println!("Connection established!");
-        let clients = connection_list.clone();
+        let clients = connection_map.clone();
 
         std::thread::spawn(move || {
             handle_connection(&stream, clients);
@@ -38,47 +48,78 @@ fn main() {
     }
 }
 
-
 // REGISTER <client_id>
 // SEND <client_id> <message>
 // BROADCAST <message>
-fn command_parser(str: String){
+fn command_parser(stream: &TcpStream, arguments: String, connections: Arc<Mutex<HashMap<String, TcpStream>>>){
+    let mut words: Vec<&str> = arguments.split(' ').collect();
+    println!("{:?}", words);
 
-    let (cmd, payload) = str.split_at(2);
-    match cmd {
-        "exit" => {
-            // send data to all clients that are connected, i quit.
+    let mut payload_list = words.split_off(1); // split into command and payload
+    
+    match words[0] {
+        "EXIT" => {
             std::process::exit(0);
         }
-        _ => {
-            println!("Unknown command");
+
+        "REGISTER" => {
+            let mut unlocked_map = connections.lock().unwrap();
+            let client_id = payload_list[0].trim().to_string();
+            println!("{}", client_id);
+
+            unlocked_map.insert(client_id, stream.try_clone().unwrap());
+            println!("{:?}", unlocked_map);
+        }
+
+        "SEND" => {
+            let message = String::from_iter(payload_list.split_off(1)); 
+            let unlocked_map = connections.lock().unwrap();
+            //payload list is now a length 1 list containing client_id
+            let target_client = payload_list[0];
+            println!("{}", target_client);
+
+            let client = unlocked_map.get(target_client); 
+            println!("{:?}", unlocked_map.keys());
+
+            client.unwrap().write_all(message.as_bytes()).unwrap();
+        }
+
+        "BROADCAST" => {
+            // send data to all clients that are connected
+            let payload = String::from_iter(payload_list);
+
+            connections.lock().unwrap().iter().for_each(|(_id, mut client)| {
+                client.write_all(payload.as_bytes()).unwrap();
+            });
+        }
+
+        &_ => {
+            println!("{}", words[0]);
         }
     }
 }
 
-fn handle_connection(mut stream: &TcpStream, connections: Arc<Mutex<Vec<TcpStream>>>) {
+fn handle_connection(mut stream: &TcpStream, connections: Arc<Mutex<HashMap<String, TcpStream>>>) {
     let mut buffer = [0; 1024];
     let timeout_duration = Duration::from_millis(200);
 
     stream.set_read_timeout(Some(timeout_duration)).unwrap();
     
     loop {
-        
         match stream.read(&mut buffer) {
             |Ok(0) => {
                 println!("Client disconnected");
                 break;
             },
-            |Ok(_size) => {
-                let my_str = String::from_utf8_lossy(&buffer[..]);
+            |Ok(size) => {
+                let my_str = String::from_utf8_lossy(&buffer[..size]);
                 let mut answer = String::new();
                 answer.push_str("> ");
                 answer.push_str(&my_str);
-
+                
+                let clients = connections.clone();
+                command_parser(stream, my_str.to_string(), clients);
                 // Spread the incoming text over all clients.
-                connections.lock().unwrap().iter().for_each(|mut client| {
-                    client.write_all(answer.as_bytes()).unwrap();
-                });
             }
             |Err(_e) => {
                 // println!("Error: {}", e);    // This gonna occur if the client doesn't send in data.
@@ -90,9 +131,10 @@ fn handle_connection(mut stream: &TcpStream, connections: Arc<Mutex<Vec<TcpStrea
 }
 
 fn get_bind_addr() -> String {
-    let maybe_arg = env::args().nth(3);
+    let maybe_arg = env::args().nth(1);
+    println!("{:?}", maybe_arg);
     match maybe_arg {
-        Some(arg) => format!("0.0.0.0:{:?}", arg),
+        Some(arg) => format!("0.0.0.0:{}", arg),
         None => "0.0.0.0:7007".to_owned()
     }
 }
