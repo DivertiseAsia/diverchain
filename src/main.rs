acf62afb097ec2b154234e18a331f1980f30c0a0
Songphon K <song@divertise.asia>
2022-06-15 11:04:05 +0700

    to make it work with tx_list

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use std::time::Duration;
 use std::net::TcpStream;
 use std::env;
 use std::fs::File;
 use std::net::TcpListener;
 use std::io::BufReader;
 use std::io::prelude::*;
+use std::sync::mpsc::{channel, Sender, Receiver};
 
 fn main() {
     let binding_addr = get_bind_addr();
@@ -17,31 +19,61 @@ fn main() {
     println!("Server is started");
     println!("You can try to connect to the server using telnet");
 
-    let mut stream_list = Vec::<TcpStream>::new();
+    let mut tx_list = Vec::<Sender<String>>::new();
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        let stream_clone = stream.try_clone().expect("Cannot clone stream"); //clone the stream
 
         println!("Connection established!");
+        let (tx, rx) = channel();
+        let x = tx_list;
 
         std::thread::spawn(move || {
-            handle_connection(&stream_clone);
+            handle_connection(&stream, &rx, &tx_list);
         });
-        
-        stream_list.push(stream);
-        
-        println!("Stream list: {:?}", stream_list);
     }
 }
 
-fn handle_connection(mut stream: &TcpStream) {
+fn handle_connection(mut stream: &TcpStream, mut rx: &Receiver<String>, tx_list: &Vec<Sender<String>>) {
     let mut buffer = [0; 1024];
     println!("We are inside the handle_connection function");
+    let timeout_duration = Duration::from_millis(200);
 
+    stream.set_read_timeout(Some(timeout_duration));
     
-    while stream.read(&mut buffer).unwrap() > 0{
-        println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+    loop {
+        
+        match stream.read(&mut buffer) {
+            |Ok(size) => {
+                if size == 0 {
+                    println!("Client disconnected");
+                    break;
+                }
+
+                let data = rx.recv_timeout(timeout_duration);
+                match data {
+                    |Ok(s) => {
+                        stream.write(s.as_bytes()).unwrap();
+                    }
+                    |Err(_) => {
+                        println!("Channel closed!?");
+                    }
+                }
+
+                let mut answer = String::new();
+                answer.push_str("> ");
+
+                let my_str = String::from_utf8_lossy(&buffer[..]);
+                println!("Request: {}", my_str);
+                answer.push_str(&my_str);
+
+                stream.write(answer.as_bytes()).unwrap();
+            }
+            |Err(e) => {
+                // println!("Error: {}", e);
+            }
+        }
+        
     }
 
     println!("Connection closed, ready for the next one");
